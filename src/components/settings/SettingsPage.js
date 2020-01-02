1dda12fe3c04a12c9f1c71c3f757507b2e9f9e18
Eleanor Lee <seungbineleanor@gmail.com>
2020-01-01 18:32:51 -0600

    Add initial values to settings page.

--- src/components/settings/SettingsPage.js
+++ src/components/settings/SettingsPage.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import SettingsForm from './SettingsForm';
 import { connect } from 'react-redux';
-import { updateSettings } from '../../actions'
+import { updateSettings } from '../../actions';
+import _ from 'lodash';
 
 class SettingsPage extends React.Component {
   render () {
     return (
       <div>
-        <h2 className="ui centered grid">My Settings</h2>
-        <SettingsForm userObject = { this.props.userObject } updateSettings = {this.props.updateSettings}/>
+        <h2 className="ui centered grid" style={{marginTop:'20px', marginBottom:'20px'}}>My Settings</h2>
+        <SettingsForm
+          initialValues = { this.props.userObject }
+          userObject = { this.props.userObject }
+          updateSettings = { this.props.updateSettings }
+        />
       </div>
     );
   }
